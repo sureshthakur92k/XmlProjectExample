5eab78aaa03f828d78b04a970ea293fd1259bfd6
Suresh Thakur <suresh.thakur92k@gmail.com>
2020-10-24 22:26:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlProjectExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
